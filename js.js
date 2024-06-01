6014ca59ac722a2f2293f5e94acd20ac956a5388
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-06-01 21:17:46 +0700

    first commit

---
+++ js.js
@@ -0,0 +1,31 @@
+document.getElementById('myForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  // Mengumpulkan nilai dari input
+  var nama = document.getElementById('textField').value;
+  var kotaAcara = document.getElementById('exampleDataList').value;
+  var tanggalAcara = document.getElementById('dateField').value;
+  var kategoriTiket = document.getElementById('selectField').value;
+  var metodePembayaran = document.querySelector('input[name="radioOptions"]:checked').value;
+  var alamat = document.getElementById('textareaField').value;
+  var fotoProfil = document.getElementById('formFile').value; // Ingat bahwa Anda perlu logika khusus untuk menangani file input
+
+  // Menampilkan nilai input dalam alert
+  var message = "Nama: " + nama + "\n";
+  message += "Kota Konser: " + kotaAcara + "\n";
+  message += "Tanggal Konser: " + tanggalAcara + "\n";
+  message += "Kategori Tiket: " + kategoriTiket + "\n";
+  message += "Metode Pembayaran: " + metodePembayaran + "\n";
+  message += "Alamat: " + alamat + "\n";
+  message += "Bukti Pembayaran: " + fotoProfil;
+
+  // Menampilkan alert dengan tombol konfirmasi dan batal
+  var confirmation = confirm(message +"\n\nApakah Anda ingin melanjutkan pendaftaran?");
+  if (confirmation) {
+    // Lakukan tindakan ketika dikonfirmasi
+    alert('Pendaftaran berhasil!');
+  } else {
+    // Lakukan tindakan ketika dibatalkan
+    alert('Pendaftaran dibatalkan.');
+  }
+});
